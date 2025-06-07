790c06d4b1d9998cf845d20b9ac69f66509327bf
Aldahiree <Aldahiree.A137431@stu.sanjac.edu>
2025-06-07 03:54:18 +0000

    Completed Lab 1 Hello-World assignment

---
+++ Hellow-World.java
@@ -0,0 +1,25 @@
+puplic class Hellow-World {
+    public static void main(string[]args) {
+        // This is the main method, the entry point of the program
+        System.out.println("Hello,World!"); // This line prints "Hello, World!" to the console
+        // This line prints my name to the console 
+        System.out.println("My name is Abdullah.");
+        // This lines prints my favorite food to the console 
+        System.out.println("My favorite food is pizza.");
+        // This line prints a celebrity I would like to meet to the console
+        System.out.println("I would like to meet Lady Gaga.");
+        // This line prints my least favorite chore to the console
+        System.out.println("My least favorite chore is doing homework");
+        // This line prints what I would rather be doing to the console
+        System.out.println ("I would rather be playing games right now");
+    }
+}
+
+/*
+1. What doest the main() method represent in a java program?
+--> It is the entry point where the program starts running.
+2. why is System.ot.println() used to display output?
+--> It sends a text to the console so users can see the program's messages.
+3. What is the purpose of comments in the program?
+--> comments explain what the code does. they help others to understand the code better
+*/
